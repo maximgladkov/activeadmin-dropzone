6735aa11cdf3ad49c29c21b5a9e612d4b2216590
Maxim Gladkov <maksim.gladkov@gmail.com>
2013-12-20 18:54:18 +0200

    misc

--- app/controllers/dropzone_controller.rb
+++ app/controllers/dropzone_controller.rb
@@ -3,7 +3,7 @@ class DropzoneController < ApplicationController
   def upload
     dropzone_class = params[:dropzone_class].constantize
     dropzone_object = dropzone_class.new
-    dropzone_object.send "#{ dropzone_class.dropzone_field }=" params[:file]
+    dropzone_object.send "#{ dropzone_class.dropzone_field }=", params[:file]
 
     if dropzone_object.save
       render json: dropzone_object.to_json

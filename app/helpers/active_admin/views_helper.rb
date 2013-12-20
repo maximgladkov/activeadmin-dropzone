842089c35ee834b61de129c22eb359706c698162
Maxim Gladkov <maksim.gladkov@gmail.com>
2013-12-20 18:32:18 +0200

    misc

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -1,6 +1,6 @@
 module ActiveAdmin::ViewsHelper
 
-  def render_mock_dropzone_objects(dropzone_objects)
+  def render_mock_dropzone_files(dropzone_objects)
     unless dropzone_objects.empty?
       dropzone_objects.map do |dropzone_object|
         "{ id: #{ dropzone_object.id }, name: '#{ dropzone_object.send dropzone_object.klass.dropzone_name_field }', size: #{ dropzone_object.send dropzone_object.klass.dropzone_file_size_field }, url: '#{ dropzone_object.send dropzone_object.klass.dropzone_url_field }' }"
--- app/inputs/dropzone_input.rb
+++ app/inputs/dropzone_input.rb
@@ -2,7 +2,7 @@ class DropzoneInput
   include Formtastic::Inputs::Base
 
   def to_html
-    template.render 'dropzone', object: object, options: options
+    template.render 'dropzone', object: object, options: options, method: method
   end
 
 end

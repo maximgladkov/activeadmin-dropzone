aa0c80fca6f8c8c3b16d8fc39ae06a97c1a00156
Maxim Gladkov <maksim.gladkov@gmail.com>
2013-12-20 19:23:40 +0200

    misc

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -3,7 +3,7 @@ module ActiveAdmin::ViewsHelper
   def render_mock_dropzone_files(dropzone_objects)
     unless dropzone_objects.empty?
       dropzone_objects.map do |dropzone_object|
-        "{ id: #{ dropzone_object.id }, name: '#{ dropzone_object.send dropzone_object.class.dropzone_name_field }', size: #{ dropzone_object.send dropzone_object.class.dropzone_file_size_field }, url: '#{ dropzone_object.send dropzone_object.class.dropzone_url_field }' }"
+        "{ id: #{ dropzone_object.id }, name: '#{ dropzone_object.send dropzone_object.class.dropzone_title_field }', size: #{ dropzone_object.send dropzone_object.class.dropzone_file_size_field }, url: '#{ dropzone_object.send dropzone_object.class.dropzone_url_field }' }"
       end.join(', ').html_safe
     end
   end

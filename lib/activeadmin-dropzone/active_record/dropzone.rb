55a8ec16988fde1c8448fe82f781d6d0984ad35c
Maxim Gladkov <maksim.gladkov@gmail.com>
2014-01-24 13:44:51 +0200

    misc

--- lib/activeadmin-dropzone/active_record/dropzone.rb
+++ lib/activeadmin-dropzone/active_record/dropzone.rb
@@ -11,9 +11,9 @@ module Activeadmin
           def #{ association_name }_attributes=(attributes)
             reflection = self.class.reflect_on_association(:#{ association_name })
 
-            p "self.class.reflect_on_association(:#{ association_name })"
-
             self.#{ association_name } = reflection.class_name.constantize.find(attributes.select{ |id, hash| !id.blank? and id != '-1' }.map{ |id, hash| id.to_i })
+
+            p self.images
 
             self.#{ association_name }.each do |dropzone_object|
               if dropzone_object.title != attributes[dropzone_object.id.to_s]['title'] or dropzone_object.position != attributes[dropzone_object.id.to_s]['position'].to_i

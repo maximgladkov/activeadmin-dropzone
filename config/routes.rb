9c6655d3991a7f903b3f59397f8a72f8ad317756
Maxim Gladkov <maksim.gladkov@gmail.com>
2014-02-24 10:45:13 +0200

    Temporary fixed double route problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # temporary fix double route error
-unless defined?(upload_dropzone_path)
+unless defined?(Rails.application.routes.url_helpers.upload_dropzone_path)
   Rails.application.routes.draw do
     
     post '/dropzone/upload' => 'dropzone#upload', as: :upload_dropzone

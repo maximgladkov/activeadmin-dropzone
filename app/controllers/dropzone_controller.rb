b305197c27b42fdcd223db3855b86b9ed4870098
Maxim Gladkov <maksim.gladkov@gmail.com>
2014-03-06 22:45:36 +0200

    Added new parameter to upload

--- app/controllers/dropzone_controller.rb
+++ app/controllers/dropzone_controller.rb
@@ -3,7 +3,8 @@ class DropzoneController < ApplicationController
   def upload
     dropzone_class = params[:dropzone_class].constantize
     dropzone_object = dropzone_class.new
-    dropzone_object.send "#{ dropzone_class.dropzone_field(:type) }=", params[:dropzonable_class] if dropzone_class.dropzone_field?(:type)
+    dropzone_object.send "#{ dropzone_class.dropzone_field(:container_type) }=", params[:dropzonable_class] if dropzone_class.dropzone_field?(:container_type)
+    dropzone_object.send "#{ dropzone_class.dropzone_field(:container_id) }=", params[:dropzonable_id] if dropzone_class.dropzone_field?(:container_id)
     dropzone_object.send "#{ dropzone_class.dropzone_field(:data) }=", params[:file]
 
     if dropzone_object.save

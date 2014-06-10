99a31f2e78b20313ee8553f2456c8299ee0b4d09
Maxim Gladkov <contact@maximgladkov.com>
2014-06-10 10:14:33 +0300

    misc

--- app/helpers/active_admin/view_helpers/dropzone_helper.rb
+++ app/helpers/active_admin/view_helpers/dropzone_helper.rb
@@ -2,10 +2,10 @@ module ActiveAdmin::ViewHelpers::DropzoneHelper
 
   def render_mock_dropzone_files(dropzone_objects)
     dropzone_objects.map do |dropzone_object|
-      { 
-        id: dropzone_object.id, 
-        name: dropzone_object.send(dropzone_object.class.dropzone_field(:title)),
-        size: dropzone_object.send(dropzone_object.class.dropzone_field(:file_size)), 
+      {
+        id: dropzone_object.id,
+        name: dropzone_object.send(dropzone_object.class.dropzone_field(:title)).squish,
+        size: dropzone_object.send(dropzone_object.class.dropzone_field(:file_size)),
         url: dropzone_object.send(dropzone_object.class.dropzone_field(:url))
       }
     end.map do |hash|
@@ -14,5 +14,5 @@ module ActiveAdmin::ViewHelpers::DropzoneHelper
       "{#{ string }}"
     end.join(', ').html_safe
   end
-  
+
 end
